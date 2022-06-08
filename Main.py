7d7824fe44082f4b6239c457eb05e0c34b7b21c0
Oscar Birging <oscarbi@student.chalmers.se>
2022-06-08 10:43:42 +0100

    Load data

--- Main.py
+++ Main.py
@@ -7,3 +7,18 @@ import time
 import math
 import matplotlib.pyplot as plt
 from yaml import load
+
+mat = sio.loadmat('PBD_Share/EurosportAll/esqueletosveryslow.mat')
+mat_complete = sio.loadmat('PBD_Share/EurosportAll/esqueletosveryslow_complete.mat')
+
+# Missing data
+skeletons = mat["skeldata"]
+frame_num = skeletons[0,:]
+x = skeletons[1::3,:]
+y = skeletons[2::3,:]
+
+# Complete data
+skeletons_c = mat_complete["skeldata"]
+frame_num_c = skeletons[0,:]
+x_c = skeletons[1::3,:]
+y_c = skeletons[2::3,:]
